f2d5ee6a9b67dc3d40fb13d6b948d508f9a01a28
BeckieMorton <beckam11@gmail.com>
2024-06-10 15:51:12 +1000

    backend endpoints with local host mongodb database working

--- server.js
+++ server.js
@@ -1,19 +1,58 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+//------Connect to database------//
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://127.0.0.1:27017/gratitudewall";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+//-----Check connection is working----//
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+//-----Define mongoose model based on gratitudes database----//
+const gratitude = mongoose.model("Gratitude", {
+  message: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 140,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
+
+//-------Defines the port the app will run on-----//
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+//--------Add middlewares-------//
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+//---------END POINTS---------//
+
+//lists all end points
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
+});
+
+//----- End point for all sites-----//
+app.get("/gratitudes", async (req, res) => {
+  const gratitudes = await gratitude.find();
+  res.json(gratitudes);
 });
 
 // Start the server
