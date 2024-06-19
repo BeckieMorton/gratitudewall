591a222a7030dcd5ec08a6d30b3a44b64459a451
BeckieMorton <beckam11@gmail.com>
2024-06-19 14:39:29 +1000

    separated back and front both are working with npm run dev

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { BrowserRouter, NavLink, Routes } from "react-router-dom";
+
+// import routes from "./routes/routes";
+
+export const App = () => {
+  return (
+    <>
+      {/* <BrowserRouter>
+        <main>
+          <Routes>{routes}</Routes>
+        </main>
+      </BrowserRouter> */}
+      <p>this is in App</p>
+    </>
+  );
+};
